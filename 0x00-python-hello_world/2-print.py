67e073b21c7bc57e6778f0b9b1c49e4cd05a0300
sai <ojiip95@gmail.com>
2023-10-30 15:09:24 +0200

    0x00: task 2, fix output & requirments checks

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-
-"""
-    Module to print TEXT 
-"""
-
-print("Programming is like building a multilingual puzzle")
+print("\"Programming is like building a multilingual puzzle")
