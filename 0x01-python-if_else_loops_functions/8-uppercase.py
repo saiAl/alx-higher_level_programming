de1fb0f448b1697974c483d070bddcbc5847cfe0
sai <ojiip95@gmail.com>
2023-10-31 17:49:40 +0200

    0x01: task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    word = ''
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            word += chr(ord(i) - 32)
+        else:
+            word += i
+    print(word)
