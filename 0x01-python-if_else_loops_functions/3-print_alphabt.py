5f6969584c4d44bc819818a7d50c0569fe60337d
sai <ojiip95@gmail.com>
2023-10-31 18:48:51 +0200

    0x01: task 2, format a string

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(97, 122):
+for i in range(97, 123):
     if chr(i) == 'q' or chr(i) == 'e':
         continue
     else:
-        print(chr(i), end='')
+        print("{:s}".format(chr(i)), end='')
