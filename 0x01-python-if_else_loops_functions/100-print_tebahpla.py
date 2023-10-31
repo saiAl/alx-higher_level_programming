9d1129239b4e1743285a41c20f9a687a6ed3bbac
sai <ojiip95@gmail.com>
2023-10-31 21:52:10 +0200

    0x01: task 100, fix checks

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
 ch = 122
-pos = 1
+pos = 0
 
 while (ch > 96):
-    if (pos % 2 == 0):
-        cap = ch - 32
-        pos = 0
-        print(chr(cap), end='')
-    else:
-        print(chr(ch), end='')
-    
     pos += 1
+
+    if (pos % 2 == 0):
+        letter = ch - 32
+        pos = 0
+    else:
+        letter = ch
+
+    print(chr(letter), end='')
     ch -= 1
