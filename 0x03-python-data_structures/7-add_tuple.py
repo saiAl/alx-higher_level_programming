e445243c32ee87361e00f378e93c740fe9e90e82
sai <ojiip95@gmail.com>
2023-11-04 15:09:56 +0200

    0x03: task 7, create another function to handle different cases

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,25 +5,26 @@ def add_tuple(tuple_a=(), tuple_b=()):
     b = list()
     new = list()
 
-    a = list(tuple_a)
-    b = list(tuple_b)
+    a = check_tuple(list(tuple_a))
+    b = check_tuple(list(tuple_b))
 
-    if len(a) < 2:
-        if len(b) == 0:
-            for i in range(1):
-                b.append(0)
-        for i in range(len(a)):
-            a.append(0)
-
-    if len(b) < 2:
-        if len(b) == 0:
-            for i in range(1):
-                b.append(0)
-        for i in range(len(b)):
-            b.append(0)
-
-    for i in range(len(a)):
+    for i in range(2):
         res = a[i] + b[i]
         new.append(res)
-
     return tuple(new)
+
+
+def check_tuple(list_c=[]):
+    new = list()
+    if len(list_c) < 2:
+        if len(list_c) == 0:
+            for i in range(1):
+                list_c.append(0)
+        for i in range(len(list_c)):
+            list_c.append(0)
+    if len(list_c) > 2:
+        for i in range(2):
+            new.append(list_c[i])
+        return new
+
+    return list(list_c)
