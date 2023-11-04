622772dcd1663c84f7b25418b94f2c237839d32c
sai <ojiip95@gmail.com>
2023-11-04 14:54:29 +0200

    0x03: task 3, handle empty list edge case

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
+
 def print_reversed_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        print()
     length = len(my_list) - 1
     while length >= 0:
         print("{:d}".format(my_list[length]))
