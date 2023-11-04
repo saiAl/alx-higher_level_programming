abca928ad1005b9adbce794105fb9d3418867f32
sai <ojiip95@gmail.com>
2023-11-04 12:59:23 +0200

    0x03: task 2, handle None edge case

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
+    if my_list == None:
+        return None
+    if idx == None:
+        return my_list
+
     my_list[idx] = element
     return my_list
