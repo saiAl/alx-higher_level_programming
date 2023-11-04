383a5709a5091d269b48d2d5054e33d4ffd27564
sai <ojiip95@gmail.com>
2023-11-04 13:01:29 +0200

    0x03: task 2, handle different edge cases

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+
 def replace_in_list(my_list, idx, element):
-    if my_list == None:
-        return None
-    if idx == None:
+    if idx < 0:
+        return my_list
+    if idx > len(my_list):
         return my_list
 
     my_list[idx] = element
