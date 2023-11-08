32b6c8c2aa68c2dfd5874c31946314a188cc8a0e
sai <ojiip95@gmail.com>
2023-11-08 17:27:22 +0200

    0x04: task 9, fix code style

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,7 +4,6 @@ def multiply_by_2(a_dictionary):
     new_list = list()
     for key, value in a_dictionary.items():
         new_list.append((key, value * 2))
-    
+
     new_dict = dict(new_list)
     return new_dict
-
