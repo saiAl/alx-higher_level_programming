a64a7b9d3a8c3151a61715dcb0af223ee54eafb0
sai <ojiip95@gmail.com>
2023-11-08 17:09:16 +0200

    0x04: task 4, fix checks

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,7 +4,15 @@ def only_diff_elements(set_1, set_2):
     set_3 = list()
 
     for i in set_1:
-        set_3.append(i)
+        if i in set_2:
+            continue
+        else:
+            set_3.append(i)
+
     for i in set_2:
-        set_3.append(i)
+        if i in set_1:
+            continue
+        else:
+            set_3.append(i)
+
     return set(set_3)
