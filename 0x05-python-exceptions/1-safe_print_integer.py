513daaee5230a6e960ecd7699cfee34ed5cf5613
sai <ojiip95@gmail.com>
2023-11-20 19:27:03 +0200

    0x05: task 1, fix except statment

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,5 +5,5 @@ def safe_print_integer(value):
         if (isinstance(value, int)):
             print("{:d}".format(value))
             return True
-    except ValueError:
+    except Exception:
         return False
