83b62619ab7350d19820c1a2069ba3fb4ed7bda1
sai <ojiip95@gmail.com>
2023-11-20 19:07:29 +0200

    0x05: task 0, fix except statment

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     num = 0
     try:
@@ -9,5 +10,5 @@ def safe_print_list(my_list=[], x=0):
                 num += 1
         print()
         return num
-    except:
+    except Exception:
         pass
