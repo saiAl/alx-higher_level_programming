59a2674a8dffe2cbcddf54b1193c36b2fd8f8934
sai <ojiip95@gmail.com>
2023-11-21 15:12:26 +0200

    0x06: task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+    5-square - module containign the Square class based on
+        4-square module/class.
+"""
+
+
+class Square:
+    """
+        Square - class of Square
+    """
+    def __init__(self, size=0):
+        """
+            size: private instance attribute.
+        """
+
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """
+            area - return the current square area
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end='')
+            print()
