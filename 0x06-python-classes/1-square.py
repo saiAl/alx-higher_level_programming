ad31c9556a09e2f2f1845afd02c287ea1050e202
sai <ojiip95@gmail.com>
2023-11-21 14:42:54 +0200

    0x06: task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+    1-square - module containign the Square class based on
+        0-square module/class.
+"""
+
+
+class Square:
+    """
+        Square - class of Square'
+    """
+    def __init__(self, size):
+        """
+            size: private instance attribute.
+        """
+        self.__size = size
