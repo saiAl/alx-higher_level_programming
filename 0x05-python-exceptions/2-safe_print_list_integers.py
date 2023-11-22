b28a699fae145b6f0a3e45b9b5c49f3d7e5c8562
sai <ojiip95@gmail.com>
2023-11-22 22:10:48 +0200

    0x05: task 2, fix the edge case

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        num = 0
-        leng = length(my_list)
-        if (x > leng):
-            raise IndexError("list index out of range")
-        for idx, value in enumerate(my_list):
-            if (idx < x):
-                if (isinstance(value, int)):
-                    print("{:d}".format(value), end='')
-                    num += 1
-        print()
-        return num
-    except TypeError:
-        pass
-
-
-def length(listt=[]):
-    idx = 0
-    for i in listt:
-        idx += 1
-    return idx
+    num = 0
+    for i in range(x):
+        try:
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end='')
+                num += 1
+        except TypeError:
+            pass
+    print()
+    return num
