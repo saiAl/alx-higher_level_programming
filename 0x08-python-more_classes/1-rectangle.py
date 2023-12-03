74bc286bd6f81d0e9610b79bd79a73377a92e807
sai <ojiip95@gmail.com>
2023-12-03 08:37:28 +0200

    handle rasies

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,6 +20,11 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
         self.__height = value
 
     @property
@@ -28,4 +33,9 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
         self.__width = value
