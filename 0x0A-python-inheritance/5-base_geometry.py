6d783e1c938d3539e058cd06da9a0bc3b133622d
sai <ojiip95@gmail.com>
2023-12-04 22:54:03 +0200

    0x0A: task 5, 6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+    Module 5-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """
+        BaseGeometry - an empty class
+    """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+    Module 6-base_geometry.py based on 5-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """
+        BaseGeometry - an empty class
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
