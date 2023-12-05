c58eb9c3e2c90bad2e24fd3ae348445ba96cf184
sai <ojiip95@gmail.com>
2023-12-05 18:38:36 +0200

    0x0B: task 2, fix pycodestyle

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,19 +4,19 @@
 
 
 def append_write(filename="", text=""):
-        """
-            append_write - a function that appends a string at the end of a text file
-                and returns the number of characters added
+    """
+        append_write - a function that appends a string at the end of
+            a text file and returns the number of characters added
 
-            Parameters:
-                filename: path to the file
-                text: text to append to the file
+        Parameters:
+            filename: path to the file
+            text: text to append to the file
 
-            Returns:
-                number of bytes added to the file
-        """
+        Returns:
+            number of bytes added to the file
+    """
 
-        with open(filename, 'a', encoding="utf-8") as f:
-            data = f.write(text)
+    with open(filename, 'a', encoding="utf-8") as f:
+        data = f.write(text)
 
-        return data
+    return data
