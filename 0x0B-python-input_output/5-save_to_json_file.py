05ab3643559462c1d6fcbad2a49c991e21571ce1
sai <ojiip95@gmail.com>
2023-12-05 18:48:10 +0200

    0x0B: task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Module 5-save_to_json_file.py """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+        save_to_json_file - a function that writes an Object to
+            a text file, using a JSON representation
+
+        Parameters:
+            my_obj: pyhton object
+            filename: path to the file.
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
