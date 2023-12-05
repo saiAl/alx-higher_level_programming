1ed3ba6e86cb6ee09327a59c6ef735054316891f
sai <ojiip95@gmail.com>
2023-12-05 08:29:16 +0200

    0x0B: task 0, document code

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python
 
+"""
+    Module 0-read_file.py
+"""
+
+
 def read_file(filename=""):
+    """
+        read_file - a function that reads a text file
+            and prints it to stdout
+
+        Parameters:
+            filename: path to the file.
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         data = f.read()
     print(data)
