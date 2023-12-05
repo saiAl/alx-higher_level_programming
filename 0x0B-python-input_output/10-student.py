842864f5f7e384ac0bebceacfcbd6db0516a6403
sai <ojiip95@gmail.com>
2023-12-05 21:27:12 +0200

    0x0B: task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+""" Module  10-student.py based on 9-student.py """
+
+
+class Student:
+    """
+        a class that defines a student by
+            first_name
+            last_name
+            age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+            retrieves a dictionary representation
+                of a Student instance
+        """
+        new = {}
+
+        if attrs is None:
+            return self.__dict__
+        else:
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    new[key] = value
+            return new
+                        
