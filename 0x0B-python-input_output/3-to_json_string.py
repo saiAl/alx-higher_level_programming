87be636c33a08a3c068d49c2a94bd224442fd36c
sai <ojiip95@gmail.com>
2023-12-05 09:41:33 +0200

    0x0B: task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" Module 3-to_json_string.py """
+
+import json
+
+def to_json_string(my_obj):
+    """
+        to_json_string - a function that returns
+            the JSON representation of an object
+
+        Parameters:
+            my_obj: python object
+
+        Returns:
+            JSON representation of the object
+    """
+
+    return json.dumps(my_obj)
