559451b5be0a842c3f238d636ee90b23caeae61b
sai <ojiip95@gmail.com>
2023-12-05 08:35:26 +0200

    0x0B: task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+"""Module 1-write_file.py"""
+
+
+def write_file(filename="", text=""):
+    """
+        write_file - a function that writes a string to a text file
+            and returns the number of characters written
+
+        Parameters:
+            filename: path to the file.
+            text: bytes to write in the file
+
+        Returns:
+            number of bytes written
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        data = f.write(text)
+
+    return data
