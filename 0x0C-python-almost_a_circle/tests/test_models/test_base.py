5f130b13fb7dcaa9a8e1a6cc057d8c69c417c493
sai <ojiip95@gmail.com>
2023-12-11 17:55:44 +0200

    0x0C: task 1, add test case file exists

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,9 +3,16 @@
 """ Tests module for tasks task 0 """
 from models.base import Base
 import unittest
+import os
+
 
 class testBase(unittest.TestCase):
     """ testBase containing test cases for Base """
+
+    def test_existing_file(self):
+        """Test if an existing file is detected"""
+        file_path = os.path.exists("models/base.py")
+        self.assertTrue(os.path.exists(file_path))
     
     def test_init_with_id(self):
         """ checks if __init__ correctly assigns the provided ID """
