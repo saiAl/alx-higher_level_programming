feeb757412265aa2240c96dc8afe19df7bb61207
sai <ojiip95@gmail.com>
2023-12-12 12:56:44 +0200

    0x0C: add square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,22 +1,62 @@
 #!usr/bin/python3
 
-""" Document this module """
+"""
+    This module defines the Square class, which inherits
+        from the Rectangle class and represents a square with
+        attributes for size, x coordinate, y coordinate, and id.
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ Document this class """
+    """
+        The Square class represents a square with attributes for size
+            (which determines both width and height), x coordinate,
+            y coordinate, and id. It inherits all functionalities
+            from the Rectangle class and adds specific methods
+            and properties related to its square shape.
+
+        methods:
+            __init__: constructor initializes the Square object
+                with the provided size, x and y coordinates,
+                and optional id. It inherits the Rectangle
+                constructor to set the width and height equal
+                to the provided size.
+
+            property width, height: property getter
+                returns the current value of the Square's size
+                attribute, which represents both its width and height.
+
+            width.setter, height.setter: This setter allows updating
+                the Square's size attribute by validating the provided
+                value It updates both the width and height
+                attributes with the new size value.
+    """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ Document the method"""
+        """ Square's class constructor """
         super().__init__(width=size, height=size, x=x, y=y, id=id)
         self.size = size
 
     @property
     def width(self):
+        """
+            getter returns the current value of the
+                Square's size attribute
+        """
         return self.__size
+
     @width.setter
     def width(self, value):
+        """
+            setter to update and validate the Square's size attribute
+
+            args:
+                value (int): the new size value
+            raises:
+                TypeError: if value not of type int
+                ValueError: if value less then 0
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -26,10 +66,24 @@ class Square(Rectangle):
 
     @property
     def height(self):
+        """
+            getter returns the current value of the
+                Square's size attribute
+        """
         return self.__size
 
     @height.setter
     def height(self, value):
+        """
+            setter to update and validate the Square's size attribute
+
+            args:
+                value (int): the new size value
+            raises:
+                TypeError: if value not of type int
+                ValueError: if value less then 0
+        """
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -50,11 +104,14 @@ class Square(Rectangle):
                 setattr(self, ["id", "size", "x", "y"][idx], arg)
         elif kwargs:
             for key, value in kwargs.items():
-                setattr(self, key, value)   
-    
+                setattr(self, key, value)
 
     def __str__(self):
-        """ Document this method """
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x, self.y, self.size)
-
-
+        """
+            method to returns a string representation of the
+                Square object, including its id, x and y
+                coordinates, and size.
+        """
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
+                self.id, self.x, self.y, self.size
+                )
