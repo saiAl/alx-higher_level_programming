cc58b5ee415702cd6031c9cfd1b15257ad787ee0
Sai <ojiip95@gmail.com>
2024-04-08 14:39:36 +0200

    0x12: task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+const converted = parseInt(argv[2]);
+
+if (isNaN(converted)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${converted}`);
+}
+
