1d123012c1d62d41edb0dfe47415b048f7ff49f6
Sai <ojiip95@gmail.com>
2024-04-09 01:37:05 +0200

    0x12: fix task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,9 +3,14 @@
 const { argv } = require('node:process');
 const arr = [];
 
-for (let x = 2; x < argv.length; x++) {
-  arr.push(argv[x]);
-}
+if (argv.length <= 3) {
+  console.log(0);
+} else {
+  for (let x = 2; x < argv.length; x++) {
+    arr.push(Number(argv[x]));
+  }
 
-const idx = arr.length - 2;
-console.log(arr.sort()[idx]);
+  const idx = arr.length - 2;
+  const sortedArr = arr.sort();
+  console.log(sortedArr[idx]);
+}
