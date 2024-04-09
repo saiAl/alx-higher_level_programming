6d386182fa88f31a3b939b9ace6cde1a94525cce
Sai <ojiip95@gmail.com>
2024-04-09 23:18:04 +0200

    0x13: task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let x = '';
+    let y = '';
+    let arr = [];
+
+    for (let idx = 0; idx < this.width; idx++) {
+      x += 'X';
+    }
+    for (let idy = 0; idy < this.height; idy++) {
+      arr.push(x);
+    }
+    arr.forEach((value) => {
+      console.log(value)
+    });
+  }
+
+  rotate () {
+    let tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+  
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
