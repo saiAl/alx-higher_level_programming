9272eb1c0b37026aebebacc1a9c096efa66c4c98
Sai <ojiip95@gmail.com>
2024-04-12 00:04:18 +0200

    0x13: task 102, modify the script to accept command line arguments

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const { argv } = require('node:process');
 
-const str1 = fs.readFileSync('fileA', 'utf-8');
-const str2 = fs.readFileSync('fileB', 'utf-8');
+const str1 = fs.readFileSync(argv[2], 'utf-8');
+const str2 = fs.readFileSync(argv[3], 'utf-8');
 const str3 = str1 + str2;
 
-fs.writeFileSync('fileC', str3);
+fs.writeFileSync(argv[4], str3);
