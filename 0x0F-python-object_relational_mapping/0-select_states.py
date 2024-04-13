8f9b2fc7966329d35f49efdc0f1d9304fa241740
Sai <ojiip95@gmail.com>
2024-04-13 19:57:22 +0200

    0x0F: task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,18 +10,23 @@ def records():
         script that lists all states from the database hbtn_0e_0_usa
 
     """
+
     try:
-        with MySQLdb.connect(
-                "localhost", sys.argv[1], sys.argv[2], sys.argv[3], port=3306
-                ) as db:
-            with db.cursor() as cur:
-                cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
-                for r in cur.fetchall():
-                    print(r)
-    except Exception as e:
-        print(e)
-        exit(0)
+        db = MySQLdb.connect(
+            "localhost", sys.argv[1], sys.argv[2], sys.argv[3], port=3306
+            )
+    except Exception as err:
+        print(err)
+        exit(1)
+
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
+    for r in cur.fetchall():
+        print(r)
+    
+    cur.close()
+    db.close()
 
 
-if '__name__' == '__main__':
-    records()
+#if '__name__' == '__main__':
+records()
