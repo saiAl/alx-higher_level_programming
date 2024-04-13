bcd4d43e7660b42ae5489137a1f9c7083b8d139e
Sai <ojiip95@gmail.com>
2024-04-13 16:51:14 +0200

    0x0F: fix task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,11 +6,14 @@ import MySQLdb
 
 def filter_by_usr_input():
     """ script that takes in an argument and displays all values """
+    usr_input = sys.argv[4]
     with MySQLdb.connect(
-            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]) as db:
+            host="localhost", user=sys.argv[1], passwd=sys.argv[2],
+            db=sys.argv[3], port=3306
+            ) as db:
         with db.cursor() as cur:
             cur.execute("""SELECT id, name FROM states
-            WHERE name LIKE %s""", (sys.argv[4],))
+            WHERE name LIKE '{:s}' ORDER BY id ASC;""".format(usr_input))
             for r in cur.fetchall():
                 print(r)
 
