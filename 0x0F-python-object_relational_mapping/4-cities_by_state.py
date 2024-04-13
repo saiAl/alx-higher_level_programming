dd94eae724e85e4d27ca7e7be828a9900bb0b9c0
Sai <ojiip95@gmail.com>
2024-04-13 23:08:35 +0200

    0x0F: task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" 4. Cities by states """
+import sys
+import MySQLdb
+
+
+def list_cities():
+    """ script that lists all cities """
+    try:
+        with MySQLdb.connect(
+                "localhost", sys.argv[1], sys.argv[2], sys.argv[3]
+                ) as db:
+            with db.cursor() as cur:
+                cur.execute(
+                        """
+                        SELECT cities.id, cities.name, states.name
+                        FROM cities INNER JOIN states
+                        ON cities.state_id = states.id ORDER BY id;
+                        """
+                        )
+                for r in cur.fetchall():
+                    print(r)
+    except Exception as err:
+        print(err)
+        exit(1)
+
+
+if '__name__' == '__main__':
+    list_cities()
