ea60f429427b48572acbd8a5200a2ea3bb8e0548
Sai <ojiip95@gmail.com>
2024-04-13 16:54:11 +0200

    0x0F: make the statement case sensative

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,12 +8,12 @@ def filter_by_usr_input():
     """ script that takes in an argument and displays all values """
     usr_input = sys.argv[4]
     with MySQLdb.connect(
-            host="localhost", user=sys.argv[1], passwd=sys.argv[2],
-            db=sys.argv[3], port=3306
+            "localhost", sys.argv[1], sys.argv[2],
+            sys.argv[3], port=3306
             ) as db:
         with db.cursor() as cur:
             cur.execute("""SELECT id, name FROM states
-            WHERE name LIKE '{:s}' ORDER BY id ASC;""".format(usr_input))
+            WHERE name = BINARY '{:s}' ORDER BY id ASC;""".format(usr_input))
             for r in cur.fetchall():
                 print(r)
 
