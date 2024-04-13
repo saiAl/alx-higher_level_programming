8b4c02b5d338040d9cf5ae7277b1fe50ccfea69f
Sai <ojiip95@gmail.com>
2024-04-13 16:59:47 +0200

    0x0F: task 2, wrap the connection in try statement

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,15 +7,18 @@ import MySQLdb
 def filter_by_usr_input():
     """ script that takes in an argument and displays all values """
     usr_input = sys.argv[4]
-    with MySQLdb.connect(
+    try:
+        with MySQLdb.connect(
             "localhost", sys.argv[1], sys.argv[2],
             sys.argv[3], port=3306
             ) as db:
-        with db.cursor() as cur:
-            cur.execute("""SELECT id, name FROM states
-            WHERE name = BINARY '{:s}' ORDER BY id ASC;""".format(usr_input))
-            for r in cur.fetchall():
-                print(r)
+            with db.cursor() as cur:
+                cur.execute("""SELECT id, name FROM states
+                WHERE name = BINARY '{:s}' ORDER BY id ASC;""".format(usr_input))
+                for r in cur.fetchall():
+                    print(r)
+    except Exception as e:
+        print(e)
 
 
 if '__name__' == '__main__':
