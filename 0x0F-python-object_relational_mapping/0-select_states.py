794e4b527e3abfc61ee1dd07f350b6d0b0c29b64
Sai <ojiip95@gmail.com>
2024-04-15 23:11:57 +0200

    0x0F: modify the script by stroing the records fetched in a list

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,15 +6,14 @@ import sys
 
 
 if '__name__' == '__main__':
-    try:
-        with MySQLdb.connect(
-                host="localhost", user=sys.argv[1],
-                passwd=sys.argv[2], db=sys.argv[3], port=3306
-        ) as db:
-            with db.cursor() as cur:
-                cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
-                for r in cur.fetchall():
-                    print(r)
-    except Exception as err:
-        print(err)
-        exit(1)
+    lst = []
+    with MySQLdb.connect(
+        host="localhost", user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3], port=3306
+    ) as db:
+        with db.cursor() as cur:
+            cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
+            for r in cur.fetchall():
+                lst.append(r)
+    for x in lst:
+        print(x)
