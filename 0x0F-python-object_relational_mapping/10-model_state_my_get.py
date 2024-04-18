a834b21cf4caf4b3f986692664e36682f07e386f
Sai <ojiip95@gmail.com>
2024-04-18 17:41:33 +0200

    0x0F: task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" 10. Get a state """
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == '__main__':
+    usrname = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    usr_input = sys.argv[4]
+
+    url = URL.create(
+            "mysql+mysqldb", usrname,
+            passwd, "localhost",
+            3306, db
+            )
+
+    engine = create_engine(url)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(
+            State.id, State.name
+            ).order_by(State.id)
+
+    for state in states:
+        if state[1] == usr_input:
+            print(state[0])
+            break
+
+    session.close()
