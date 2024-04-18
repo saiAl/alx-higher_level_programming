aaa9381fec268cceff7288c42bd1bf6f94a4b7f8
Sai <ojiip95@gmail.com>
2024-04-18 16:53:53 +0200

    0x0F: fix equality sign, task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 import sys
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     engine = create_engine(
             "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
                 sys.argv[1], sys.argv[2], sys.argv[3], pool_pre_ping=True)
