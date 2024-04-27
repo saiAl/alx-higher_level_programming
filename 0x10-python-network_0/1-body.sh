b4a2f6ed064a0a62827af697c0dcbb4288c5c7a1
Sai <ojiip95@gmail.com>
2024-04-27 09:35:58 +0200

    0x10: task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 #  script that takes in a URL, sends a GET request to the URL
-# and displays the body of the response
-
-body=$(curl -sL "$1")
-echo "$body"
+curl -sL "$1"
