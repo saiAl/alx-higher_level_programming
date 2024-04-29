42c28370cb2caaefdcbef1f3b7dbd83ab7cc1588
Sai <ojiip95@gmail.com>
2024-04-29 20:17:14 +0200

    0x10: task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays all HTTP methods the server will accept
-body=$(curl -s -i -X OPTION "$1" | grep "Allow:") && echo "${body#*Allow: }"
+res=$(curl -sI 0.0.0.0:5000/route_4 | grep "^Allow" | cut -d ':' -f 2) && echo "$res"
