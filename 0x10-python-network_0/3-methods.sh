64371f0f5ff53da4c7eb2551424f261914c59873
Sai <ojiip95@gmail.com>
2024-04-29 20:23:41 +0200

    0x10: task 3, removing leading whitespace

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays all HTTP methods the server will accept
-res=$(curl -sI 0.0.0.0:5000/route_4 | grep "^Allow" | cut -d ':' -f 2) && echo "$res"
+res=$(curl -sI 0.0.0.0:5000/route_4 | grep "^Allow" | cut -d ':' -f 2 | xargs) && echo "$res"
