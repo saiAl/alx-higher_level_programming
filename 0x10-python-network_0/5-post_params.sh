eb73ba707dd8fc4cd306fc7311cba1e9ae2e678a
Sai <ojiip95@gmail.com>
2024-04-29 20:49:28 +0200

    0x10: task 5, add -s flag

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send POST request with data
-curl --data-urlencode "email=test@gmail.com" --data-urlencode "subject=I will always be here for PLD" "$1"
+curl --data-urlencode "email=test@gmail.com" --data-urlencode "subject=I will always be here for PLD" -s "$1"
